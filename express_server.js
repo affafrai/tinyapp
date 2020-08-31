bf022d665d4eb3a115ef21e302fe82e1d2da96d9
affaf <affaf.rai@hotmail.com>
2020-08-31 21:56:55 +0000

    add get fetch + get set

--- express_server.js
+++ express_server.js
@@ -17,6 +17,14 @@ app.get("/", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
   });
 
+  app.get("/set", (req, res) => {
+    const a = 1;
+    res.send(`a = ${a}`);
+   });
+   
+   app.get("/fetch", (req, res) => {
+    res.send(`a = ${a}`);
+   });
 });
 
 app.listen(PORT, () => {
