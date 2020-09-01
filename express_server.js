d7b108c9c8b31d87a0d25c6dc1e81da392a379c6
affaf <affaf.rai@hotmail.com>
2020-09-01 02:32:39 +0000

    add form submit by using post + generate random string function

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.set("view engine", "ejs")
 
@@ -34,13 +37,34 @@ app.get("/fetch", (req, res) => {
     res.render("urls_index", templateVars);
   });
 
+app.get("/urls/new", (req,res) => {
+    res.render("urls_new");
+  });
+
   app.get("/urls/:shortURL", (req, res) => {
     let templateVars ={ shortURL : req.params.shortURL , longURL : urlDatabase[req.params.shortURL]};
     console.log(req.params)
     res.render("urls_show", templateVars)
    
   });
+  app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
+  
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+function generateRandomString(strLength, arr) {
+  var ans = ''; 
+            for (var i = strLength; i > 0; i--) { 
+                ans +=  
+                  arr[Math.floor(Math.random() * arr.length)]; 
+            } 
+            return ans; 
+        } 
+
+console.log(generateRandomString(6,["1","r","t","4","5"]))
