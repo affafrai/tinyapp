583131fcfe9302d7233f3b39cf20f00f14592a55
affaf <affaf.rai@hotmail.com>
2020-09-01 01:39:19 +0000

    add partial header file + update index and show and express file

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs")
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -9,23 +12,34 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!\n");
-
-  app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
-  app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
-
-  app.get("/set", (req, res) => {
-    const a = 1;
-    res.send(`a = ${a}`);
-   });
-   
-   app.get("/fetch", (req, res) => {
-    res.send(`a = ${a}`);
-   });
+  // app.get("/urls.json", (req, res) => {
+  //   res.json(urlDatabase);
 });
+  
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+});
+   
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+});
+   
+   app.get("/urls", (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+
+  app.get("/urls/:shortURL", (req, res) => {
+    let templateVars ={ shortURL : req.params.shortURL , longURL : urlDatabase[req.params.shortURL]};
+    console.log(req.params)
+    res.render("urls_show", templateVars)
+   
+  });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
